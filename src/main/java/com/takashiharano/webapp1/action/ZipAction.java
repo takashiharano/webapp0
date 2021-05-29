403f85a811349e27b63b972cdc320e318388a0f2
Takashi Harano <takashiharano@users.noreply.github.com>
2021-05-30 00:06:29 +0900

    Add ZipAction

---
+++ src/main/java/com/takashiharano/webapp1/action/ZipAction.java
@@ -0,0 +1,16 @@
+package com.takashiharano.webapp1.action;
+
+import com.takashiharano.util.zip.ZipUtil;
+import com.takashiharano.webapp1.ProcessContext;
+
+public class ZipAction extends Action {
+
+  @Override
+  public void process(ProcessContext context) throws Exception {
+    String pathToZip = "C:/test/dir1";
+    String fileName = "file.zip";
+    byte[] b = ZipUtil.zip(pathToZip);
+    context.sendByteStreamResponse(b, fileName);
+  }
+
+}
