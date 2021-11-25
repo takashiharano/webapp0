ae740f7cdb8eb6af8ea8f778de08b19593c7d2a0
Takashi Harano <takashiharano@users.noreply.github.com>
2021-11-26 08:08:54 +0900

    Improve http callback

--- src/main/webapp/common/app.js
+++ src/main/webapp/common/app.js
@@ -1,13 +1,24 @@
 app = {};
 
-app.callServerApi = function(actionName, params, cb, errCb) {
+app.callServerApi = function(actionName, params, onsuccess, onerror) {
   if (!params) params = {};
   params.action = actionName;
   var req = {
     url: 'main',
     method: 'POST',
     data: params,
-    cb: cb
+    onsuccess: onsuccess,
+    onerror: onerror,
+    orgOnsuccess: onsuccess
   };
   util.http(req);
+  return req;
+};
+
+app.onHttpReceive = function(xhr, res, req) {
+  if ((res instanceof Object) && (res.status == 'FORBIDDEN')) {
+    location.href = 'main';
+    return;
+  }
+  req.orgOnsuccess(xhr, res, req);
 };
