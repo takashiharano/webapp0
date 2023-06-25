36814d4449535ce1932d2b0ad530c445a4090c35
Takashi Harano <takashiharano@users.noreply.github.com>
2023-06-25 20:58:42 +0900

    Update source code headers

--- src/main/java/com/takashiharano/webapp0/action/system/users/EditUserAction.java
+++ src/main/java/com/takashiharano/webapp0/action/system/users/EditUserAction.java
@@ -1,5 +1,7 @@
 /*
  * THIS CODE IS IMPLEMENTED BASED ON THE WEBAPP0 TEMPLATE.
+ * The template is released under the MIT license.
+ * Copyright 2023 Takashi Harano
  */
 package com.takashiharano.webapp0.action.system.users;
 
